11d0543fb62e06e900c7e4b929b3e77292621031
Tagizade Samir <tagizade.samir@gmail.com>
2023-11-27 14:16:30 +0400

    local

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import {App} from './App.jsx'
+import {App} from './AppNew.jsx'
+// import {App} from './AppOld.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
